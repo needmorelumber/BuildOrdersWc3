2d87853b15ba93a06603f8f0a00c2a56901dad95
Shimmyshanga <luca.junghans@gmail.com>
2018-04-18 12:28:50 -0700

    login image

--- components/Home.js
+++ components/Home.js
@@ -7,18 +7,19 @@ class Home extends Component {
     return (
       <div className="hero is-fullheight mainHero">
         <div className="hero-body">
-          <div className="container has-text-centered">
+          <div className="container">
             <div className="columns is-vcentered mainHomeContainer">
               <div className="column is-6 is-offset-1">
-                <h1 className="title is-2">
-                  Time to shine in battle
+                <h1 className="title mainTitle">
+                  Time to shine
                 </h1>
+                <hr/> 
                 <h2 className="subtitle is-4">
                   Be your own warcraft 3 commander with orders every second
                 </h2>
                 <br/>
                 <p className="has-text-centered">
-                  <Link className="button is-large is-dark is-outlined" to="/builds">
+                  <Link className="button is-dark is-outlined buildsButton" to="/builds">
                     Check out all the builds
                   </Link>
                 </p>
