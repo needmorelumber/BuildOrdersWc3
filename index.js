f95053020d68a2d0d96f5e129a1dcf895be20f7c
Alex Hubers <ahubers15@cornellcollege.edu>
2019-03-04 18:13:19 -0600

    Fix everything in root + store

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.sass';
 import Root from './containers/Root';
@@ -13,5 +11,5 @@ ReactDOM.render(
   <BrowserRouter>
     <Root className="main" store={store} />
   </BrowserRouter>,
-  document.getElementById('root'),
+  document.getElementById('root'), // eslint-disable-line no-undef
 );
