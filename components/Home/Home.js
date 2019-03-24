ff26708ec2ccff9a3b29fb003e9c9c16eb9bac5a
Tanner Stirrat <tstirrat@gmail.com>
2019-03-24 12:26:29 -0600

    Undo classness

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,53 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './home.sass';
 
-class Home extends Component {
-  render() {
-    return (
-      <div className="hero is-fullheight mainHero">
-        <div className="hero-body">
-          <div className="container">
-            <div className="columns is-vcentered mainHomeContainer">
-              <div className="column is-6 is-offset-1">
-                <h1 className="title mainTitle">
-                 Need More Lumber
-                </h1>
-                <hr />
-                <hr />
-                <h2 className="subtitle has-text-centered">
-                  Be your own warcraft 3 commander with orders every second,
-                  just create a build, then run it in real time during the game!
-                  Never miss the next unit to build!
-                </h2>
-                <br />
-                <p className="">
-                  <Link className="button is-info buildsButton" to="/builds">
-                    Check out all the builds
-                  </Link>
-                  <Link className="button is-primary buildsButton" to="/register">
-                    Get registered and create
-                  </Link>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="hero-foot">
-          <div className="container has-text-centered">
-            <p className="control">
-              <a className="button" href="https://github.com/LJunghansCode/BuildOrdersWc3" target="_blank" rel="noopener">
-                <span className="icon is-small">
-                  <i className="fa fa-github" />
-                </span>
-                <span>Check out the project</span>
-              </a>
+const Home = (
+  <div className="hero is-fullheight mainHero">
+    <div className="hero-body">
+      <div className="container">
+        <div className="columns is-vcentered mainHomeContainer">
+          <div className="column is-6 is-offset-1">
+            <h1 className="title mainTitle">
+             Need More Lumber
+            </h1>
+            <hr />
+            <hr />
+            <h2 className="subtitle has-text-centered">
+              Be your own warcraft 3 commander with orders every second,
+              just create a build, then run it in real time during the game!
+              Never miss the next unit to build!
+            </h2>
+            <br />
+            <p className="">
+              <Link className="button is-info buildsButton" to="/builds">
+                Check out all the builds
+              </Link>
+              <Link className="button is-primary buildsButton" to="/register">
+                Get registered and create
+              </Link>
             </p>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+    <div className="hero-foot">
+      <div className="container has-text-centered">
+        <p className="control">
+          <a className="button" href="https://github.com/LJunghansCode/BuildOrdersWc3" target="_blank" rel="noopener noreferrer">
+            <span className="icon is-small">
+              <i className="fa fa-github" />
+            </span>
+            <span>Check out the project</span>
+          </a>
+        </p>
+      </div>
+    </div>
+  </div>
+);
 
 export default Home;
