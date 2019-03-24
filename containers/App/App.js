9cda962af446098bfbb3747e7b2813c6f3bd0908
Tanner Stirrat <tstirrat@gmail.com>
2019-03-24 12:18:23 -0600

    Refactor away from state, remove component

--- containers/App/App.js
+++ containers/App/App.js
@@ -1,107 +1,40 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Link, Redirect, Switch, CSSTransitionGroup } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { normalize, schema } from 'normalizr';
-import { NavigationDrawer } from 'react-md';
+import React, { Fragment } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import BuildsPage from '../BuildsPage';
 import Header from '../../components/Header/Header';
 import NewBuild from '../../components/NewBuild/NewBuild';
-import CurrentBuild from '../CurrentBuild';
 import TimeLineContainer from '../TimeLineContainer';
 import NoMatch from '../../components/noMatch';
 import LoginPage from '../LoginPage';
 import RegisterPage from '../RegisterPage';
 import UserPage from '../UserPage';
-import AddOrder from '../../components/BuildSingle/AddOrder';
 import Home from '../../components/Home/Home';
 import InGameHelper from '../InGameHelper';
 import EditBuildPage from '../EditBuildPage';
-import Footer from '../../components/Footer/Footer';
+// import Footer from '../../components/Footer/Footer';
 import './app.sass';
 
 
-const RouteAndSub = route => (
-
-  <Route
-    path={route.path}
-    render={props => (
-      // nesting
-      <route.component {...props} routes={route.routes} />
-    )}
-  />
+const App = () => (
+  <Fragment>
+    <Header />
+    <div className="container">
+      <Switch>
+        <Redirect from="/" to="/home" />
+        <Route path="/home" component={Home} exact />
+        <Route path="/builds/new" component={NewBuild} />
+        <Route path="/builds" component={BuildsPage} exact />
+        <Route path="/build/:id" component={TimeLineContainer} exact />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/user/profile" component={UserPage} />
+        <Route path="/build/:id/playing" component={InGameHelper} />
+        <Route path="/build/:id/edit" component={EditBuildPage} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+    {/* <Footer /> */}
+  </Fragment>
 );
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      routes: [
-        {
-          path: '/home',
-          exact: true,
-          component: Home,
-        },
-        {
-          path: '/builds/new',
-          component: NewBuild,
-        },
-        {
-          path: '/builds',
-          exact: true,
-          component: BuildsPage,
-        },
-        {
-          path: '/build/:id',
-          exact: true,
-          component: TimeLineContainer,
-        },
-        {
-          path: '/login',
-          component: LoginPage,
-        },
-        {
-          path: '/register',
-          component: RegisterPage,
-        },
-        {
-          path: '/user/profile',
-          component: UserPage,
-        },
-        {
-          path: '/build/:id/playing',
-          component: InGameHelper,
-        },
-        {
-          path: '/build/:id/edit',
-          component: EditBuildPage,
-        },
-      ],
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="container">
-
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => (
-                <Redirect to="/home" />
-              )}
-            />
-            {this.state.routes.map((route, i) => (
-              <RouteAndSub key={i} {...route} />
-            ))}
-            <Route component={NoMatch} />
-          </Switch>
-        </div>
-        {/* <Footer /> */}
-      </div>
-    );
-  }
-}
 
 export default App;
