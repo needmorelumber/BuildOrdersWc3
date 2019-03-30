16b1bf0d13a383796f18a8aa60323e6b2c74a988
Alex Hubers <ahubers15@cornellcollege.edu>
2019-03-30 12:48:11 -0500

    Schema definition

--- server/models/builds/build.js
+++ server/models/builds/build.js
@@ -4,7 +4,7 @@ const buildStepSchema = new mongoose.Schema({
   food: { type: String, required: true, trim: true },
   totalFood: { type: String, required: true, trim: true },
   description: { type: String, required: true },
-  order: Number,
+  order: { type: Number, required: true },
 }, { timestamp: true });
 
 const buildSchema = new mongoose.Schema({
